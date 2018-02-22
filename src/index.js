9adb29345b4af0089279e77f0d08d61259016b78
Dmitrii Sorin <dsorin@atlassian.com>
2018-02-23 10:17:34 +1100

    Export const, remove default export (lots of conflicts with node projects)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import table from './table';
 
-export default (md, options) => {
+export const MarkdownItTable = (md, options) => {
   md.block.ruler.before('paragraph', 'table', table, {
     alt: ['paragraph', 'reference'],
   });
