794bcbb43d8d14fe56723556dbf961dfb32733bd
Rifat Nabi <to.rifat@gmail.com>
2023-07-08 01:08:27 +1000

    Update infra

---
+++ .lintstagedrc.js
@@ -0,0 +1,3 @@
+export default {
+  '*.{js}': 'vitest related --run',
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import table from './table';
+import table from "./table.js";
 
 export const markdownItTable = (md, options) => {
-  md.block.ruler.before('paragraph', 'table', table, {
-    alt: ['paragraph', 'reference'],
+  md.block.ruler.before("paragraph", "table", table, {
+    alt: ["paragraph", "reference"],
   });
 };
